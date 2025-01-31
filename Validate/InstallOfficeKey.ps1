9ade259f2fd7f3d35cc246f406c67375c1621b1f
mggons93 <malagonsxp@hotmail.com>
2025-01-31 10:54:46 -0500

    Update InstallOfficeKey.ps1

--- Validate/InstallOfficeKey.ps1
+++ Validate/InstallOfficeKey.ps1
@@ -1,4 +1,3 @@
-# Verificar si el script se está ejecutando como administrador
 function Test-Admin {
     $currentIdentity = [Security.Principal.WindowsIdentity]::GetCurrent()
     $principal = New-Object Security.Principal.WindowsPrincipal($currentIdentity)
